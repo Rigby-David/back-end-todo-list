6c60c5457d191d03a329f68e7c9d899852dab6af
David <Rigby335@hotmail.com>
2022-08-23 10:08:57 -0700

    added users test file w/ mockUser, registerAndLogin function and #POST test create new user is RED

---
+++ __tests__/users.test.js
@@ -0,0 +1,51 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
+
+//   // Create an "agent" that gives us the ability
+//   // to store cookies between requests in a test
+//   const agent = request.agent(app);
+
+//   // Create a user to sign in with
+//   // const user = await UserService.create({ ...mockUser, ...userProps });
+
+//   // ...then sign in
+//   // const { email } = user;
+//   const resp = await agent
+//     .post('/api/v1/users')
+//     .send({ ...mockUser, ...userProps });
+//   const user = resp.body;
+//   return [agent, user];
+// };
+
+describe('user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+});
